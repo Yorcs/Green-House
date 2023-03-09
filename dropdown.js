9343a7922b415f88f4ac1fb4210587d9203da187
charlotte <cdoo@sfu.ca>
2023-03-08 16:04:16 -0800

    Added hamburger menu
    - added js
    - added images
    - changed nav for all

---
+++ dropdown.js
@@ -0,0 +1,11 @@
+
+function myFunction() {
+    var checkBox = document.getElementById("myCheck");
+    var text = document.getElementById("dropdown");
+    if (checkBox.checked == true){
+    dropdown.style.display = "block";
+    } 
+    else {
+        dropdown.style.display = "none";
+    }
+}
